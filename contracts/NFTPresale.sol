0c245ea7ce49c161daeb2f0905b3144a0c582cea
shobhitvishnoi30 <vishnoishobhit201@gmail.com>
2022-09-19 08:42:14 +0530

    test:added testcases for buying nft in presale

--- contracts/NFTPresale.sol
+++ contracts/NFTPresale.sol
@@ -6,154 +6,132 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
 contract NFTPresale is ERC721Enumerable, Ownable, Pausable {
-    using Strings for uint256;
+  using Strings for uint256;
 
-    uint256 private _normalCost = 0.5 ether;
+  uint256 private _normalCost = 0.5 ether;
 
-    uint256 private _presaleCost = 0.25 ether;
+  uint256 private _presaleCost = 0.25 ether;
 
-    uint256 private _deploymentTime;
+  string public baseURI;
 
-    string public baseURI;
+  string public baseExtension;
 
-    string public baseExtension;
+  uint256 public maximumNFTSupply = 100;
 
-    uint256 public maximumNFTSupply = 100;
+  uint256 public maximumNFTAllowedInPresale = 2;
 
-    uint256 public maximumNFTAllowedInPresale = 2;
+  uint256 public presaleOpenTime;
 
-    uint256 public presaleOpenTime;
+  uint256 public presaleDuration;
 
-    uint256 public presaleDuration;
+  mapping(address => bool) public whitelistedAddresses;
 
-    mapping(address => bool) public whitelistedAddresses;
+  mapping(address => uint256) public addressMintedBalance;
 
-    mapping(address => uint256) public addressMintedBalance;
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    string memory _initBaseURI,
+    uint256 _presaleDuration
+  ) ERC721(_name, _symbol) {
+    setBaseURI(_initBaseURI);
+    presaleDuration = _presaleDuration;
+  }
 
-    event PresaleStarted(string _presaleStarted);
+  // internal
 
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        string memory _initBaseURI,
-        uint256 _presaleDuration
-    ) ERC721(_name, _symbol) {
-        setBaseURI(_initBaseURI);
-        _deploymentTime = block.timestamp;
-        presaleDuration = _presaleDuration;
+  function setPresaleOpenTime(uint256 _presaleOpenTime) external onlyOwner {
+    presaleOpenTime = _presaleOpenTime;
+  }
+
+  // public
+  function mint(uint256 _mintAmount) external payable whenNotPaused {
+    uint256 supply = totalSupply();
+    require(_mintAmount > 0, "need to mint at least 1 NFT");
+    require(supply + _mintAmount <= maximumNFTSupply, "max NFT limit exceeded");
+    require(block.timestamp >= presaleOpenTime, "too soon");
+    if (block.timestamp <= presaleOpenTime + presaleDuration) {
+      require(whitelistedAddresses[msg.sender], "user is not whitelisted");
+      uint256 ownerMintedCount = addressMintedBalance[msg.sender];
+      require(
+        ownerMintedCount + _mintAmount <= maximumNFTAllowedInPresale,
+        "max NFT per address exceeded"
+      );
+      require(msg.value >= _presaleCost * _mintAmount, "insufficient funds");
+    } else if (block.timestamp > presaleOpenTime + presaleDuration) {
+      require(msg.value >= _normalCost * _mintAmount, "insufficient funds");
     }
 
-    // internal
-
-    function setPresaleOpenTime(uint256 _presaleOpenTime) external onlyOwner {
-        presaleOpenTime = _presaleOpenTime;
-        emit PresaleStarted("Presale Started");
+    for (uint256 i = 1; i <= _mintAmount; i++) {
+      addressMintedBalance[msg.sender]++;
+      _safeMint(msg.sender, supply + i);
     }
+  }
 
-    // public
-    function mint(uint256 _mintAmount) external payable whenNotPaused {
-        uint256 supply = totalSupply();
-        require(_mintAmount > 0, "need to mint at least 1 NFT");
-        require(
-            supply + _mintAmount <= maximumNFTSupply,
-            "max NFT limit exceeded"
-        );
-        require(block.timestamp >= presaleOpenTime, "too soon");
-        if (block.timestamp <= presaleOpenTime + presaleDuration) {
-            require(
-                whitelistedAddresses[msg.sender],
-                "user is not whitelisted"
-            );
-            uint256 ownerMintedCount = addressMintedBalance[msg.sender];
-            require(
-                ownerMintedCount + _mintAmount <= maximumNFTAllowedInPresale,
-                "max NFT per address exceeded"
-            );
-            require(
-                msg.value >= _presaleCost * _mintAmount,
-                "insufficient funds"
-            );
-        } else if (block.timestamp > presaleOpenTime + presaleDuration) {
-            require(
-                msg.value >= _normalCost * _mintAmount,
-                "insufficient funds"
-            );
-        }
+  function setBaseExtension(string memory _newBaseExtension)
+    external
+    onlyOwner
+  {
+    baseExtension = _newBaseExtension;
+  }
 
-        for (uint256 i = 1; i <= _mintAmount; i++) {
-            addressMintedBalance[msg.sender]++;
-            _safeMint(msg.sender, supply + i);
-        }
+  function pause() external onlyOwner whenNotPaused {
+    _pause();
+  }
+
+  function unpause() external onlyOwner whenPaused {
+    _unpause();
+  }
+
+  function getPrice() external view returns (uint256) {
+    if (block.timestamp > presaleOpenTime + presaleDuration) {
+      return _normalCost;
+    } else {
+      return _presaleCost;
     }
+  }
 
-    function setBaseExtension(string memory _newBaseExtension)
-        external
-        onlyOwner
-    {
-        baseExtension = _newBaseExtension;
+  function whitelistUsers(address[] memory _users) external onlyOwner {
+    for (uint256 i = 0; i < _users.length; i++) {
+      if (_users[i] != address(0)) {
+        whitelistedAddresses[_users[i]] = true;
+      }
     }
+  }
 
-    function pause() external onlyOwner whenNotPaused {
-        _pause();
-    }
+  function removeWhitelistUsers(address _users) external onlyOwner {
+    whitelistedAddresses[_users] = false;
+  }
 
-    function unpause() external onlyOwner whenPaused {
-        _unpause();
-    }
+  function withdraw() external onlyOwner {
+    (bool success, ) = payable(owner()).call{ value: address(this).balance }(
+      ""
+    );
+    require(success, "transfer not succesful");
+  }
 
-    function getPrice() external view returns (uint256) {
-        if (_deploymentTime <= presaleOpenTime + presaleDuration) {
-            return _normalCost;
-        } else {
-            return _presaleCost;
-        }
-    }
+  function setBaseURI(string memory _newBaseURI) public onlyOwner {
+    baseURI = _newBaseURI;
+  }
 
-    function whitelistUsers(address[] memory _users) external onlyOwner {
-        for (uint256 i = 0; i < _users.length; i++) {
-            if (_users[i] != address(0)) {
-                whitelistedAddresses[_users[i]] = true;
-            }
-        }
-    }
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+    require(_exists(tokenId), "nonExsitent token");
+    string memory currentBaseURI = _baseURI();
+    return
+      bytes(currentBaseURI).length > 0
+        ? string(
+          abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension)
+        )
+        : "";
+  }
 
-    function removeWhitelistUsers(address _users) external onlyOwner {
-        whitelistedAddresses[_users] = false;
-    }
-
-    function withdraw() external onlyOwner {
-        (bool success, ) = payable(owner()).call{value: address(this).balance}(
-            ""
-        );
-        require(success, "transfer not succesful");
-    }
-
-    function setBaseURI(string memory _newBaseURI) public onlyOwner {
-        baseURI = _newBaseURI;
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        virtual
-        override
-        returns (string memory)
-    {
-        require(_exists(tokenId), "nonExsitent token");
-        string memory currentBaseURI = _baseURI();
-        return
-            bytes(currentBaseURI).length > 0
-                ? string(
-                    abi.encodePacked(
-                        currentBaseURI,
-                        tokenId.toString(),
-                        baseExtension
-                    )
-                )
-                : "";
-    }
-
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseURI;
-    }
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
+  }
 }
--- test/sample-test.js
+++ test/sample-test.js
@@ -11,10 +11,58 @@ async function getSigners() {
   user3 = accounts[2];
 }
 
+async function Testing() {
+  const NFT = await ethers.getContractFactory("NFTPresale");
+  const nft = await NFT.deploy("NFTPresale", "NPS", "google.com", 8);
+  await nft.deployed();
+
+  describe("Testing NFT Presale Contract", function () {
+    it("only owner should be able to set presale time", async function () {
+      expect(await nft.owner()).to.equal(user1.address);
+
+      await expect(
+        nft.connect(user2).setPresaleOpenTime("10000")
+      ).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+
+    it("owner should be able to set presale time", async function () {
+      expect(await nft.presaleOpenTime()).to.equal(0);
+
+      let time = Math.floor(Date.now() / 1000);
+
+      await nft.connect(user1).setPresaleOpenTime(time);
+
+      expect(await nft.presaleOpenTime()).to.equal(time);
+    });
+
+    it("In presale time price should be less", async function () {
+      expect(await nft.getPrice()).to.equal("250000000000000000");
+    });
+
+    it("Only whitelisted addresses can buy NFT", async function () {
+      expect(await nft.whitelistedAddresses(user2.address)).to.equal(false);
+
+      await expect(nft.connect(user2).mint(1)).to.be.revertedWith(
+        "user is not whitelisted"
+      );
+    });
+
+    it("whitelisted addresses can buy NFT", async function () {
+      await nft.whitelistUsers([user2.address]);
+      expect(await nft.whitelistedAddresses(user2.address)).to.equal(true);
+
+      expect(await nft.balanceOf(user2.address)).to.equal(0);
+
+      await nft.connect(user2).mint(1, { value: "250000000000000000" });
+
+      expect(await nft.balanceOf(user2.address)).to.equal(1);
+    });
+  });
+}
 
 describe("Setup & Test Contracts", async function () {
   it("Setting up the contracts & Testing", async function () {
     await getSigners();
-   
+    await Testing();
   }).timedOut("200000s");
 });
